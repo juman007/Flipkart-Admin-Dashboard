49201c7d4e4cf375450df7904f9b17082aaf8890
Juman Saikia <jumansaikia10@gmail.com>
2024-06-25 13:56:42 +0530

    update logout api

--- src/actions/auth.Action.js
+++ src/actions/auth.Action.js
@@ -59,9 +59,25 @@ export const isUserLoggedIn = () => {
 
 export const signout = () => {
    return async (dispatch) => {
-      window.localStorage.clear();
       dispatch({
          type: authConstants.LOGOUT_REQUEST,
       });
+
+      const res = await axios.post(`admin/signout`);
+
+      if (res.status === 200) {
+         window.localStorage.clear();
+
+         dispatch({
+            type: authConstants.LOGOUT_SUCCESS,
+         });
+      } else {
+         dispatch({
+            type: authConstants.LOGOUT_FAILURE,
+            payload: {
+               error: "Failed to logout",
+            },
+         });
+      }
    };
 };
